c5d24007e3cad31998af82bc03e98dfb78cfd817
tnthuy <thuytn@emotiv.vn>
2017-04-24 16:02:26 +0700

    update

--- suite_EmotivPro/tst_login/test.py
+++ suite_EmotivPro/tst_login/test.py
@@ -9,15 +9,16 @@ def main():
     type(waitForObject(":textField_TextField"), "<Tab>")
     type(waitForObject(":textField_TextField_2"), "Thuyvy12")
     mouseClick(waitForObject(":background_Rectangle"), 116, 14, 33554432, Qt.LeftButton)
-   
     snooze(5.6)
-    #closeWindow(":_QQuickApplicationWindow")
+    
+    #enter license
     setWindowState(waitForObject(":_QQuickApplicationWindow"), WindowState.Maximize)
     mouseClick(waitForObject(":textField_TextField"), 68, 19, 33554432, Qt.LeftButton)
     type(waitForObject(":textField_TextField"), "251fba0d-8d0b-4136-80d1-a1a84c7bc5a4")
     mouseClick(waitForObject(":background_Rectangle"), 116, 14, 33554432, Qt.LeftButton)
     snooze(15)
-    #closeWindow(":_QQuickApplicationWindow")
+    
+    #click next for the fit headset screen
     mouseClick(waitForObject(":background_Rectangle_2"), 24, 16, 100663296, Qt.LeftButton)
     doubleClick(waitForObject(":background_Rectangle_2"), 24, 13, 100663296, Qt.LeftButton)
     mouseClick(waitForObject(":background_Rectangle_3"), 143, 26, 100663296, Qt.LeftButton)
